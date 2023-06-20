4932f52361b5c826b111657bcbf3853361c59a00
Sergio H Caliani <shcaliani@gmail.com>
2023-06-20 17:42:15 -0300

    Create sell_commands.sh

---
+++ sell_commands.sh
@@ -0,0 +1,31 @@
+🟥 SHELL Comands 🟥
+
+ls -la  | lista arquivos diretório
+ls      | lista arquivos
+ll      | lista arquivos do diretório
+df      | Mostra a quantidade de espaço usada no disco rígido.
+top     | Mostra o uso da memória.
+cd      | Acessa uma determinada pasta (diretório)
+mkdir   | Cria um diretório.
+rmdir   | Apaga um diretorio
+rm      | Remove um arquivo/diretório.
+cat     | Abre um arquivo.
+cp      | Copia arquivos
+diff    | Compara o conteúdo de dois arquivos ASCII
+mv      | Move um arquivo
+uniq    | Reporta ou apaga linhas repetidas num arquivo
+wc      | Conta linhas, palavras e mesmo caracteres num arquivo
+fold    | Encurta, ou seja, faz um fold das linhas longas para caberem no dispositivo de output
+head    | Mostra as primeiras linhas de um arquivo, como por exemplo com head -10 a.txt, ou usado como filtro para mostrar apenas os primeiros x resultados de outro comando
+ping    | Pingar um determinado host
+jobs    | Permite-nos visualizar jobs em execução, quando corremos uma aplicação em background,
+kill    | Mata um processo, como por exemplo kill -kill 100 ou kill -9 100 ou kill -9 %1
+bg      | Coloca um processo suspenso em background
+date    | Exibe a data e hora
+history | Lista os últimos comandos usados, muito útil para lembrar também de que comandos foram usados
+who     | Mostra-nos quem está logado no sistema
+sleep 9 | Aguarda o tempo que indicar
+touch   | Criar arquivos
+time    | Na frente do comando, monitorar o tempo de execução nas dimensões [time xx.sh ou time ls]
+cut     | Corta o texto conforme definiçao [cut -c2]
+ps      | Lista os processos      [ps | cut -c7]
