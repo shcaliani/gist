a43845eec2c855a6d5e17bdbfbcba45cc9607f32
Sergio H Caliani <shcaliani@gmail.com>
2023-06-20 19:50:32 -0300

    Create pyenv.sh

---
+++ pyenv.sh
@@ -0,0 +1,12 @@
+
+🚩 brew para utilização do Pyenv
+
+Brew install
+
+Brew update : atualiza a lista
+Brew upgrade : atualiza os pacotes
+
+Pyenv : local + versão na pasta
+Pyenv : versions
+Pyenv Install + número da versão
+Pyenv install —List : ver versões existentes
