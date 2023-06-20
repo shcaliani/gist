0216cde0181d3235b0228f7e893cabbf0cd864ae
Sergio H Caliani <shcaliani@gmail.com>
2023-06-20 17:38:59 -0300

    Create pip_drops.sh

---
+++ pip_drops.sh
@@ -0,0 +1,13 @@
+🟥 PIP dicas 🟥
+
+https://pypi.org/
+
+||| atualizar versão do PIP 
+
+python -m pip install --upgrade pip 
+ 
+'importante: de preferencia na venv do projeto,  para evitar que bibliotecas do Python do Linux ou MacOS sejam subscritas' 
+ 
+||| instalando bibliotecas
+ 
+pip install <nome biblioteca>
