b1ddabf8bac9ece2e5494e2a91968a99ddaa51ea
Sergio H Caliani <shcaliani@gmail.com>
2023-06-22 16:29:59 -0300

    Update pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -10,3 +10,9 @@ Pyenv : local + versão na pasta
 Pyenv : versions
 Pyenv Install + número da versão
 Pyenv install —List : ver versões existentes
+⭕
+# PyEnv
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
